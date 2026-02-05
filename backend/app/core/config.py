335bb061b2f143d1a1ca8ac61c284e8ee893326c
Twisuki <Twisuki@outlook.com>
2026-02-06 00:42:50 +0800

    :sparkles: member

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,6 +14,7 @@ class AppConfig(BaseSettings):
         SECRET_KEY (str): 用于加密的密钥
         TIME_OUT (int): 请求超时时间，单位秒
         INIT_PASSWORD (str): 初始管理员密码
+        DEFAULT_PASSWORD (str): 默认密码, 用于重置用户密码
     """
 
     PORT: int = 8080
@@ -23,6 +24,7 @@ class AppConfig(BaseSettings):
     SECRET_KEY: str = ""
     TIME_OUT: int = 60 * 60
     INIT_PASSWORD: str = ""
+    DEFAULT_PASSWORD: str = ""
 
     class Config:
         env_file = ".env"
@@ -34,6 +36,10 @@ class AppConfig(BaseSettings):
             raise Exception("SECRET_KEY 未设置，请在 .env 文件中设置 SECRET_KEY")
         if self.INIT_PASSWORD == "":
             raise Exception("INIT_PASSWORD 未设置，请在 .env 文件中设置 INIT_PASSWORD")
+        if self.DEFAULT_PASSWORD == "":
+            raise Exception(
+                "DEFAULT_PASSWORD 未设置，请在 .env 文件中设置 DEFAULT_PASSWORD"
+            )
 
 
 CONFIG = AppConfig()
