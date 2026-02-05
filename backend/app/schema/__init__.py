7a0e3bbd5c6331bf546afb3f17c59f3f596b726c
Twisuki <Twisuki@outlook.com>
2026-02-06 00:11:11 +0800

    :sparkles: 封装响应体

--- backend/app/schema/__init__.py
+++ backend/app/schema/__init__.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional, TypeVar, Generic, List
 
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
@@ -8,7 +9,22 @@ T = TypeVar("T", bound=BaseModel)
 
 class BaseResponse(BaseModel, Generic[T]):
     code: str
-    data: Optional[T] = None
+    data: Optional[T]
+
+
+class Response(BaseResponse, Generic[T]):
+    def __init__(self, data: Optional[T] = None):
+        super().__init__(
+            code="OK",
+            data=data,
+        )
+
+
+class ErrorResponse(HTTPException):
+    def __init__(self, status_code: int = 500, code: str = "INTERNAL_SERVER_ERROR"):
+        super().__init__(
+            status_code=status_code, detail=BaseResponse(code=code, data=None)
+        )
 
 
 class BaseDepartment(BaseModel):
