df5649ff0e1b7022239d9b36e936f70c4fbec430
Twisuki <Twisuki@outlook.com>
2026-02-23 12:23:45 +0800

    :bug: 修复后端序列化问题

--- backend/app/schema/department.py
+++ backend/app/schema/department.py
@@ -15,8 +15,8 @@ AddDepartmentRequest = BaseDepartmentInfo
 
 
 class AddDepartmentMemberRequest(BaseModel):
-    qq_id: str = Field(..., serialization_alias="QQID")
+    qq_id: str = Field(..., alias="QQID")
 
 
 class AddDepartmentMinisterRequest(BaseModel):
-    qq_id: str = Field(..., serialization_alias="QQID")
+    qq_id: str = Field(..., alias="QQID")
--- backend/app/schema/superadmin.py
+++ backend/app/schema/superadmin.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, Field
 
 
 class AddAdminRequest(BaseModel):
-    qq_id: str = Field(..., serialization_alias="QQID")
+    qq_id: str = Field(..., alias="QQID")
 
 
 class AddSuperAdminRequest(BaseModel):
-    qq_id: str = Field(..., serialization_alias="QQID")
+    qq_id: str = Field(..., alias="QQID")
